1ebc52c8e124886a5e6e99ac3e5d3e09a2e202a9
student <seanhsu82@gmail.com>
2026-05-20 17:26:02 +0800

    test

---
+++ 0520_pandas_1.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+stock1 = pd.Series([120, 80, None, 60, 95, None, 110])
+
+stock2 = pd.Series(
+    [120, 80, None, 60, 95, None, 110],
+    index=["Apple", "Banana", "Orange", "Mango", "Grape", "Peach", "Melon"]
+)
+
+stock3 = stock2.to_dict()
+
+print("stock1")
+print(stock1)
+
+print("\nstock2")
+print(stock2)
+
+print("\nstock3")
+print(stock3)
+
+print("\nBanana 庫存：", stock2["Banana"])
+
+print("\n缺失值檢查：")
+print(stock2.isnull())
+
+print("\n缺失值數量：", stock2.isnull().sum())
+
+stock2.to_csv("0520_stock.csv")
